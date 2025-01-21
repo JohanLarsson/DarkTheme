3d6dcbe3f0300c21390b34aa5e44144c626fb7c7
Johan Larsson <johan_gavle1@hotmail.com>
2025-01-21 15:35:49 +0100

    sample  controls

---
+++ DarkTheme/Item.cs
@@ -0,0 +1,47 @@
+﻿namespace DarkTheme;
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+public class Item : INotifyPropertyChanged
+{
+    private string _name;
+    private int _score;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            if (value == _name) return;
+            _name = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public int Score
+    {
+        get => _score;
+        set
+        {
+            if (value == _score) return;
+            _score = value;
+            OnPropertyChanged();
+        }
+    }
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
---
+++ DarkTheme/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace DarkTheme;
+
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+public class MainViewModel : INotifyPropertyChanged
+{
+    private bool _isChecked;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public ObservableCollection<Item> Items { get; } = [
+        new Item{ Name = "Chad", Score = 5 },
+        new Item{ Name = "Virgin", Score = 1 },
+    ];
+
+    public bool IsChecked
+    {
+        get => _isChecked;
+        set
+        {
+            if (value == _isChecked) return;
+            _isChecked = value;
+            OnPropertyChanged();
+        }
+    }
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
