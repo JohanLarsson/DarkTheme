83bff4c71054da87b5037a5d0917212fd574bd76
Johan Larsson <johan_gavle1@hotmail.com>
2025-01-22 10:28:02 +0100

    theme

---
+++ DarkTheme/Resources/Controls.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace DarkTheme;
+
+using System.Windows;
+
+public partial class Controls
+{
+    private void OnMinimizeClick(object sender, RoutedEventArgs e)
+    {
+        if (e.Source is FrameworkElement fe &&
+            Window.GetWindow(fe) is { } window)
+        {
+            window.WindowState = WindowState.Minimized;
+        }
+    }
+
+    private void OnNormalClick(object sender, RoutedEventArgs e)
+    {
+        if (e.Source is FrameworkElement fe &&
+            Window.GetWindow(fe) is { } window)
+        {
+            window.WindowState = WindowState.Normal;
+        }
+    }
+
+    private void OnMaximizeClick(object sender, RoutedEventArgs e)
+    {
+        if (e.Source is FrameworkElement fe &&
+            Window.GetWindow(fe) is { } window)
+        {
+            window.WindowState = WindowState.Maximized;
+        }
+    }
+
+    private void OnCloseClick(object sender, RoutedEventArgs e)
+    {
+        if (e.Source is FrameworkElement fe &&
+            Window.GetWindow(fe) is { } window)
+        {
+            window.Close();
+        }
+    }
+}
